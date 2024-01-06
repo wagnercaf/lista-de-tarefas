7ea8655efd66e85d4d72874b38e9b87445485929
Wagner Cândido de Albuquerque Ferreira <wagnercaf@hotmail.com>
2024-01-06 02:07:25 -0300

    desfazer

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const { ApolloServer} = require("apollo-server");
 
-const {typeDefs, resolvers} = require("graphql/index");
+const {typeDefs, resolvers} = require("./src/graphql");
+/*
+const { makeExecutableSchema } = require('@graphql-tools/schema'); //necessário instalar antes -> npm i @graphql-tools/schema
 
+console.log(typeDefs);
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+ 
+const server = new ApolloServer({schema});
+*/
 const server = new ApolloServer({    
     typeDefs, resolvers
 });
 
-server.listen().then(({URL}) => console.log(URL));
+server.listen().then(({ url }) => console.log( url ));
+
+
--- src/graphql/modules/usuario/resolvers.js
+++ src/graphql/modules/usuario/resolvers.js
@@ -1,23 +1,15 @@
 const db = require("../../../db");
 
 module.exports = {
-
-    Usuario:{
-        telefone(Obj){
-           // console.log(Obj);
-            return Obj.telefone_fixo;
-        },
-        
-        perfil(usuario){
-            return db.perfis.find(p => p.id == usuario.id);
-        }
+  Usuario: {
+    perfil(usuario) {
+      return db.perfis.find((p) => p.id === usuario.perfil_id);
     },
-
-    Query : {
-        usuario(Obj , args){
-            return db.usuarios.find((db) => db.id== args.id);
-        },
-
-        usuarios:() => db.usuarios,
+  },
+  Query: {
+    usuario(obj, args) {
+      return db.usuarios.find((db) => db.id === args.id);
     },
+    usuarios: () => db.usuarios,
+  },
 };
